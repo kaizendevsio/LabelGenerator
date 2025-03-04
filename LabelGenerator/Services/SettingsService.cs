faf0093d15000bd66744edcf92cdfcc399585430
Jay Eraldo <jjeeraldo@gmail.com>
2025-03-04 21:14:03 +0800

    Bug fixes and improvements

--- LabelGenerator/Services/SettingsService.cs
+++ LabelGenerator/Services/SettingsService.cs
@@ -5,20 +5,28 @@ namespace LabelGenerator.Services;
 
 public class SettingsService
 {
-    public DynamicSize PageSize { get; init; } = new() { DynamicWidth = "8.5in", DynamicHeight = "11in" };
+    public DynamicSize PageSize { get; set; } = new() { DynamicWidth = "1.27cm", DynamicHeight = "1.5cm" };
     private DynamicSize? PageSizeClone { get; set; } = new();
     public DynamicSize QrSize { get; init; } = new() { DynamicWidth = "1.2cm" };
     public string QrCodeColorInput { get; set; } = "#000000"; // default QR code color
-    public bool SingleQrPerPage
+
+    private bool _usePrinterDefaults = true;
+    public bool UsePrinterDefaults
     {
-        get;
+        get => _usePrinterDefaults;
         set
         {
             if (value)
             {
                 PageSizeClone = PageSize.Adapt<DynamicSize>();
-                
+                PageSize = new() { DynamicWidth = "1.27cm", DynamicHeight = "1.5cm" };
             }
+            else
+            {
+                PageSize = PageSizeClone.Adapt<DynamicSize>();
+            }
+
+            _usePrinterDefaults = value;
         }
     }
 }
